8ae2a3c99c5e42e2f4e446228242d21e11acfc5f
Gabriel Rivera <gabriel.rivera70@upr.edu>
2018-12-28 23:16:01 -0400

    added simple drawing capability

--- draw.js
+++ draw.js
@@ -2,29 +2,71 @@ var canvas = document.getElementById("myCanvas"); //sets canvas as an object of 
 
 var context = canvas.getContext("2d");//sets context as a something we can manipulate
 
-context.fillStyle = "red";
+// The pencil tool instance
+tool = new tool_pencil();
 
-//context.fillRect(50,50,140,75);
+// Attach the mousedown, mousemove and mouseup event listeners
+canvas.addEventListener('mousedown', ev_canvas, false);
+canvas.addEventListener('mousemove', ev_canvas, false);
+canvas.addEventListener('mouseup',	 ev_canvas, false);
+canvas.setAttribute('width','800');
+canvas.setAttribute('height', '600');
+console.log(canvas.width);
+console.log(canvas.height);
+// The mousemove event handler
+var started = false;
 
-var Point = (function () { //Point podemos hacerlo a que sea igual a context y si eventhandler es true pues pinta
-    function Point(x, y, time) {
-        this.x = x;
-        this.y = y;
-        this.time = time || Date.now();
-    }
-    Point.prototype.distanceTo = function (start) {
-        return Math.sqrt(Math.pow(this.x - start.x, 2) + Math.pow(this.y - start.y, 2));
-    };
-    Point.prototype.equals = function (other) {
-        return this.x === other.x && this.y === other.y && this.time === other.time;
-    };
-    Point.prototype.velocityFrom = function (start) {
-        return this.time !== start.time
-            ? this.distanceTo(start) / (this.time - start.time)
-            : 0;
-    };
-    return Point;
-}());
+function tool_pencil () {
+	var tool = this;
+	this.started = false;
+
+	// This is called when you start holding down the mouse button
+	// This starts the pencil drawing
+	this.mousedown = function (ev) {
+			context.beginPath();
+			context.moveTo(ev._x, ev._y);
+			tool.started = true;
+	};
+
+	// This function is called every time you move the mouse. Obviously, it only
+	// draws if the tool.started state is set to true (when you are holding down
+	// the mouse button)
+	this.mousemove = function (ev) {
+		if (tool.started) {
+			context.lineTo(ev._x, ev._y);
+			context.stroke();
+		}
+	};
+
+	// This is called when you release the mouse button
+	this.mouseup = function (ev) {
+		if (tool.started) {
+			tool.mousemove(ev);
+			tool.started = false;
+		}
+	};
+}
+
+function ev_canvas (ev) {
+	// Firefox
+	if (ev.layerX || ev.layerX == 0) {
+		ev._x = ev.layerX;
+		ev._y = ev.layerY;
+	// Opera
+	} else if (ev.offsetX || ev.offsetX == 0) {
+		ev._x = ev.offsetX;
+		ev._y = ev.offsetY;
+	}
+
+	// Call the event handler of the tool
+	var func = tool[ev.type];
+	if (func) {
+		func(ev);
+	}
+}
+
+
+
   
 
 
