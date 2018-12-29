dbe813638320f4b0cae8536177cd64d47c4e5859
Gabriel Rivera <gabriel.rivera70@upr.edu>
2018-12-28 22:36:46 -0400

    understanding the canvas

---
+++ draw.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("myCanvas"); //sets canvas as an object of canvas from the html
+
+var context = canvas.getContext("2d");//sets context as a something we can manipulate
+
+context.fillStyle = "red";
+
+context.fillRect(50,50,140,75);
+
